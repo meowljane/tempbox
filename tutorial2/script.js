e3e8dca2f91aba7d09a4f0cc6d0d5ac6f57626d4
meowljane <69661367+meowljane@users.noreply.github.com>
2021-01-06 10:36:03 +0900

    Add files via upload

---
+++ tutorial2/script.js
@@ -0,0 +1,42 @@
+$('#content')
+  .on("dragover", dragOver)
+  .on("dragleave", dragOver)
+  .on("drop", uploadFiles);
+
+function dragOver(e){
+	e.stopPropagation();
+	e.preventDefault();
+	if (e.type == "dragover") {
+		$(e.target).css({
+			"background-color": "black",
+			"outline-offset": "-20px"
+		});
+	} else {
+    	$(e.target).css({
+			"background-color": "black",
+			"outline-offset": "-10px"
+		});
+    }
+}
+
+function uploadFiles(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dragOver(e);
+  
+    e.dataTransfer = e.originalEvent.dataTransfer;
+    var files = e.target.files || e.dataTransfer.files;
+    if (files.length > 1) {
+        alert('하나만 올려주세요.');
+        return;
+    }
+
+if (files[0].name.match(/키워드열쇠.png/)){
+$('#키워드열쇠').css({"display":"block"}).draggable();
+}
+
+else{
+      alert('올바른 아이템을 올려주세요.');
+      return;
+    }
+}
